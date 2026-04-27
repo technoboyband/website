8c9e36a9f7fa4c7d3e101312799a343687cf2682
technoboyband <jabyyp@gmail.com>
2026-04-27 14:39:17 -0700

    set up proj

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>gabs was here</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
