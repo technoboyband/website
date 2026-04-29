1b744cf853cb2f60fbf2bc46402d504f84d7d8a3
technoboyband <jabyyp@gmail.com>
2026-04-29 14:00:57 -0700

    add da pikachu gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './App.css'
 import { Link } from 'react-router'
+import underConstructionGif from './assets/under-construction.gif'
 
 function App() {
-
   return (
     <>
       <section id="center">
         <div>
           <h1>gabs was here</h1>
-          <Link to="/thoughts">thoughts</Link>
+          <img src={underConstructionGif}/>
         </div>
+        <Link to="/thoughts">thoughts</Link>
       </section>
     </>
   )
