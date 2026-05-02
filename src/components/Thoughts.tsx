59e41db0ce4eec7e033fd1f8290f60fbfbe95db3
technoboyband <jabyyp@gmail.com>
2026-05-01 19:37:50 -0700

    add second blog

--- src/components/Thoughts.tsx
+++ src/components/Thoughts.tsx
@@ -10,14 +10,16 @@ export default function Thoughts() {
     <h1>thoughts</h1>
       <section id="center">
         <div>
-          <p>blog 1 body</p>
+          <p>blog bodies</p>
         </div>
       </section>
       <aside className='sidebar'>
         <h4>sidebar</h4>
         <ol>
           <li><a href="https://techno.bearblog.dev/vhsdevlog1/">
-          <p>blog 1</p></a></li>
+          blog 1</a></li>
+          <li><a href="https://techno.bearblog.dev/vhs-shelf-dev-log-2/">
+          blog 2</a></li>
         </ol>
       </aside>
       <Link to="/">home</Link>
